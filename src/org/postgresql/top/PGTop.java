00a7b55a29420c6f7530400efc873e0a1716ea7c
Mark Wong <markwkm@gmail.com>
2010-07-03 11:55:03 -0700

    start monitoring OnClickListener
    Implement the OnClickListener for starting the database monitoring as
    part of the activity class.  This is supposed to avoid an extra class
    load and object allocation.

--- src/org/postgresql/top/PGTop.java
+++ src/org/postgresql/top/PGTop.java
@@ -15,7 +15,67 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PGTop extends Activity {
+public class PGTop extends Activity implements OnClickListener {
+	public void onClick(View v) {
+		final EditText hostEditText = (EditText) findViewById(R.id.host);
+		final EditText portEditText = (EditText) findViewById(R.id.port);
+		final EditText dbnameEditText = (EditText) findViewById(R.id.dbname);
+		final EditText userEditText = (EditText) findViewById(R.id.user);
+		final EditText passwordEditText = (EditText) findViewById(R.id.password);
+
+		setContentView(R.layout.display);
+
+		String PGHost = hostEditText.getText().toString();
+		String PGPort = portEditText.getText().toString();
+		String DBName = dbnameEditText.getText().toString();
+		String PGUser = userEditText.getText().toString();
+		String PGPassword = passwordEditText.getText().toString();
+
+		TextView connectionsTextView = (TextView) findViewById(R.id.connections);
+
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			setContentView(R.layout.main);
+			Toast.makeText(PGTop.this, e.toString(), Toast.LENGTH_LONG).show();
+			return;
+		}
+
+		String url = "jdbc:postgresql:";
+		if (PGHost.length() > 0) {
+			url += "//" + PGHost;
+			if (PGPort.length() > 0) {
+				url += ":" + PGPort;
+			}
+			url += "/";
+		}
+		url += DBName;
+
+		try {
+			Connection conn = DriverManager.getConnection(url, PGUser,
+					PGPassword);
+
+			Statement st = conn.createStatement();
+			ResultSet rs = st
+					.executeQuery("SELECT numbackends FROM pg_stat_database WHERE datname = '"
+							+ DBName + "'");
+			if (rs.next()) {
+				connectionsTextView.setText("Database Connections: "
+						+ rs.getString(1));
+			} else {
+				connectionsTextView.setText("Database Connections:");
+			}
+			rs.close();
+			st.close();
+
+			conn.close();
+		} catch (SQLException e) {
+			setContentView(R.layout.main);
+			Toast.makeText(PGTop.this, e.toString(), Toast.LENGTH_LONG).show();
+			return;
+		}
+	}
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,68 +83,6 @@ public class PGTop extends Activity {
 		setContentView(R.layout.main);
 
 		final Button startButton = (Button) findViewById(R.id.start);
-		final EditText hostEditText = (EditText) findViewById(R.id.host);
-		final EditText portEditText = (EditText) findViewById(R.id.port);
-		final EditText dbnameEditText = (EditText) findViewById(R.id.dbname);
-		final EditText userEditText = (EditText) findViewById(R.id.user);
-		final EditText passwordEditText = (EditText) findViewById(R.id.password);
-
-		startButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				setContentView(R.layout.display);
-
-				String PGHost = hostEditText.getText().toString();
-				String PGPort = portEditText.getText().toString();
-				String DBName = dbnameEditText.getText().toString();
-				String PGUser = userEditText.getText().toString();
-				String PGPassword = passwordEditText.getText().toString();
-
-				TextView connectionsTextView = (TextView) findViewById(R.id.connections);
-
-				try {
-					Class.forName("org.postgresql.Driver");
-				} catch (ClassNotFoundException e) {
-					setContentView(R.layout.main);
-					Toast.makeText(PGTop.this, e.toString(), Toast.LENGTH_LONG)
-							.show();
-					return;
-				}
-
-				String url = "jdbc:postgresql:";
-				if (PGHost.length() > 0) {
-					url += "//" + PGHost;
-					if (PGPort.length() > 0) {
-						url += ":" + PGPort;
-					}
-					url += "/";
-				}
-				url += DBName;
-
-				try {
-					Connection conn = DriverManager.getConnection(url, PGUser,
-							PGPassword);
-
-					Statement st = conn.createStatement();
-					ResultSet rs = st
-							.executeQuery("SELECT numbackends FROM pg_stat_database WHERE datname = '"
-									+ DBName + "'");
-					if (rs.next()) {
-						connectionsTextView.setText("Database Connections: "
-								+ rs.getString(1));
-					} else {
-						connectionsTextView.setText("Database Connections:");
-					}
-					rs.close();
-					st.close();
-
-					conn.close();
-				} catch (SQLException e) {
-					setContentView(R.layout.main);
-					Toast.makeText(PGTop.this, e.toString(), Toast.LENGTH_LONG)
-							.show();
-					return;
-				}
-			}
-		});
+		startButton.setOnClickListener(this);
 	}
 }
