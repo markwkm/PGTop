9034bba0246e7ddebb55473f1d8329aaf4a7ed42
Mark Wong <markwkm@gmail.com>
2010-07-02 21:12:13 -0700

    add button listener
    Added a listener for the click of the "Start Monitoring" button.  This
    button then closes the app.

--- src/org/postgresql/top/PGTop.java
+++ src/org/postgresql/top/PGTop.java
@@ -2,12 +2,21 @@ package org.postgresql.top;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class PGTop extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		final Button startButton = (Button) findViewById(R.id.start);
+		startButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+	}
 }
