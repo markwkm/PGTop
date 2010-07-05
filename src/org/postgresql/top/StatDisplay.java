23bcb72a05841117b1f209d835ca66b8e46c3bcc
Mark Wong <markwkm@gmail.com>
2010-07-04 19:54:22 -0700

    hard coded refresh
    Hard coded a 2 second refresh rate for the database statistics
    displayed.  Hopefully handled onResume(), onPause(), onStop(), and
    onDestroy() properly.  Used 3 values for the enum on handling what the
    state of execution is, but because of the limited nature of the Thread
    class implemented in Dalvik virtual machine and if I understand things
    correctly, this probably only needs to be a boolean value.

--- src/org/postgresql/top/StatDisplay.java
+++ src/org/postgresql/top/StatDisplay.java
@@ -10,13 +10,34 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class StatDisplay extends Activity implements OnClickListener {
+public class StatDisplay extends Activity implements OnClickListener, Runnable {
+	private String DBName;
+	private String url;
+	private String PGUser;
+	private String PGPassword;
+
+	private TextView headerTextView;
+	private TextView connectionsTextView;
+
+	private String headerString;
+	private String backendString;
+
+	Thread thread;
+
+	private enum State {
+		RUNNING, PAUSED, EXITING
+	};
+
+	private State state;
+
 	public void onClick(View view) {
 		Intent intent = new Intent();
 		setResult(RESULT_OK, intent);
@@ -28,11 +49,10 @@ public class StatDisplay extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.display);
 
+		state = State.RUNNING;
+
 		final Button stopButton = (Button) findViewById(R.id.stop);
 		stopButton.setOnClickListener(this);
-
-		TextView headerTextView = (TextView) findViewById(R.id.displayheader);
-		TextView connectionsTextView = (TextView) findViewById(R.id.connections);
 
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -43,34 +63,83 @@ public class StatDisplay extends Activity implements OnClickListener {
 		}
 
 		SharedPreferences settings = getSharedPreferences("PGTopPrefs", 0);
-		String DBName = settings.getString("dbname", "");
-		String url = settings.getString("pgurl", "");
-		String PGUser = settings.getString("pguser", "");
-		String PGPassword = settings.getString("pgpassword", "");
+		DBName = settings.getString("dbname", "");
+		url = settings.getString("pgurl", "");
+		PGUser = settings.getString("pguser", "");
+		PGPassword = settings.getString("pgpassword", "");
 
-		try {
-			Connection conn;
-			Statement st;
-			ResultSet rs;
+		headerTextView = (TextView) findViewById(R.id.displayheader);
+		connectionsTextView = (TextView) findViewById(R.id.connections);
 
-			conn = DriverManager.getConnection(url, PGUser, PGPassword);
+		thread = new Thread(this);
+	}
 
-			st = conn.createStatement();
-			rs = st
-					.executeQuery("SELECT NOW(), numbackends FROM pg_stat_database WHERE datname = '"
-							+ DBName + "'");
-			if (rs.next()) {
-				headerTextView.setText(DBName + " " + rs.getString(1));
-				connectionsTextView.setText("Database Connections: "
-						+ rs.getString(2));
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		state = State.EXITING;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		state = State.PAUSED;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		state = State.RUNNING;
+		thread.start();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		state = State.EXITING;
+	}
+
+	public void run() {
+		while (state == State.RUNNING) {
+			try {
+				Connection conn;
+				Statement st;
+				ResultSet rs;
+
+				conn = DriverManager.getConnection(url, PGUser, PGPassword);
+
+				st = conn.createStatement();
+				rs = st
+						.executeQuery("SELECT NOW(), numbackends FROM pg_stat_database WHERE datname = '"
+								+ DBName + "'");
+				if (rs.next()) {
+					headerString = DBName + " " + rs.getString(1);
+					backendString = "Database Connections: " + rs.getString(2);
+				}
+				rs.close();
+				st.close();
+				conn.close();
+
+				handler.sendEmptyMessage(0);
+				// FIXME: Make the refresh rate a configuration parameter.
+				Thread.sleep(2000);
+			} catch (SQLException e) {
+				Toast.makeText(StatDisplay.this, e.toString(),
+						Toast.LENGTH_LONG).show();
+				return;
+			} catch (InterruptedException e) {
+				Toast.makeText(StatDisplay.this, e.toString(),
+						Toast.LENGTH_LONG).show();
+				return;
 			}
-			rs.close();
-			st.close();
-			conn.close();
-		} catch (SQLException e) {
-			Toast.makeText(StatDisplay.this, e.toString(), Toast.LENGTH_LONG)
-					.show();
-			return;
 		}
 	}
+
+	private Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			headerTextView.setText(headerString);
+			connectionsTextView.setText(backendString);
+		}
+	};
 }
