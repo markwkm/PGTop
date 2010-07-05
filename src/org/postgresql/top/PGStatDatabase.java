e798d15d15bbeeb96c5fdf1a59319c460142d3d2
Mark Wong <markwkm@gmail.com>
2010-07-05 08:43:56 -0700

    display all pg_stat_database stats
    Display all the stats from pg_stat_database for a particular database.  Also
    converts the blocks read and hit into number of bytes as well as database
    blocks.

--- src/org/postgresql/top/PGStatDatabase.java
+++ src/org/postgresql/top/PGStatDatabase.java
@@ -26,10 +26,41 @@ public class PGStatDatabase extends Activity implements OnClickListener,
 	private String pgPassword;
 
 	private TextView headerTextView;
-	private TextView connectionsTextView;
+	private TextView numbackendsTextView;
+	private TextView commitsTextView;
+	private TextView rollbacksTextView;
+	private TextView readTextView;
+	private TextView hitTextView;
+	private TextView returnedTextView;
+	private TextView fetchedTextView;
+	private TextView insertedTextView;
+	private TextView updatedTextView;
+	private TextView deletedTextView;
 
 	private String headerString;
-	private String backendString;
+	private long numbackends = 0;
+	private long commits = 0;
+	private long rollbacks = 0;
+	private long read = 0;
+	private long hit = 0;
+	private long returned = 0;
+	private long fetched = 0;
+	private long inserted = 0;
+	private long updated = 0;
+	private long deleted = 0;
+
+	private long commitsOld = 0;
+	private long rollbacksOld = 0;
+	private long readOld = 0;
+	private long hitOld = 0;
+	private long returnedOld = 0;
+	private long fetchedOld = 0;
+	private long insertedOld = 0;
+	private long updatedOld = 0;
+	private long deletedOld = 0;
+
+	private String readPretty;
+	private String hitPretty;
 
 	Thread thread;
 
@@ -38,6 +69,61 @@ public class PGStatDatabase extends Activity implements OnClickListener,
 	};
 
 	private State state;
+
+	private void getDatabaseStats() throws SQLException {
+		Connection conn;
+		Statement st;
+		ResultSet rs;
+
+		String sql = ""
+				+ "SELECT NOW(), numbackends, xact_commit, xact_rollback, "
+				+ "          blks_read, blks_hit, tup_returned, tup_fetched, "
+				+ "          tup_inserted, tup_updated, tup_deleted, "
+				+ "          PG_SIZE_PRETTY((blks_read - "
+				+ Long.toString(readOld) + ") * setting), "
+				+ "          PG_SIZE_PRETTY((blks_hit - "
+				+ Long.toString(hitOld) + ") * setting) "
+				+ "FROM (SELECT setting::BIGINT "
+				+ "      FROM pg_settings "
+				+ "      WHERE name = 'block_size') AS pg_settings, "
+				+ "     pg_stat_database "
+				+ "WHERE datname = '" + pgDatabase + "';";
+
+		conn = DriverManager.getConnection(url, pgUser, pgPassword);
+
+		st = conn.createStatement();
+		rs = st.executeQuery(sql);
+		if (rs.next()) {
+			/* Save previous values. */
+			commitsOld = commits;
+			rollbacksOld = rollbacks;
+			readOld = read;
+			hitOld = hit;
+			returnedOld = returned;
+			fetchedOld = fetched;
+			insertedOld = inserted;
+			updatedOld = updated;
+			deletedOld = deleted;
+
+			/* Get new values. */
+			headerString = pgDatabase + " " + rs.getString(1);
+			numbackends = rs.getLong(2);
+			commits = rs.getLong(3);
+			rollbacks = rs.getLong(4);
+			read = rs.getLong(5);
+			hit = rs.getLong(6);
+			returned = rs.getLong(7);
+			fetched = rs.getLong(8);
+			inserted = rs.getLong(9);
+			updated = rs.getLong(10);
+			deleted = rs.getLong(11);
+			readPretty = rs.getString(12);
+			hitPretty = rs.getString(13);
+		}
+		rs.close();
+		st.close();
+		conn.close();
+	}
 
 	public void onClick(View view) {
 		Intent intent = new Intent();
@@ -71,7 +157,16 @@ public class PGStatDatabase extends Activity implements OnClickListener,
 		pgPassword = settings.getString("pgpassword", "");
 
 		headerTextView = (TextView) findViewById(R.id.displayheader);
-		connectionsTextView = (TextView) findViewById(R.id.connections);
+		numbackendsTextView = (TextView) findViewById(R.id.numbackends);
+		commitsTextView = (TextView) findViewById(R.id.xact_commit);
+		rollbacksTextView = (TextView) findViewById(R.id.xact_rollback);
+		readTextView = (TextView) findViewById(R.id.blks_read);
+		hitTextView = (TextView) findViewById(R.id.blks_hit);
+		returnedTextView = (TextView) findViewById(R.id.tup_returned);
+		fetchedTextView = (TextView) findViewById(R.id.tup_fetched);
+		insertedTextView = (TextView) findViewById(R.id.tup_inserted);
+		updatedTextView = (TextView) findViewById(R.id.tup_updated);
+		deletedTextView = (TextView) findViewById(R.id.tup_deleted);
 
 		thread = new Thread(this);
 	}
@@ -108,23 +203,7 @@ public class PGStatDatabase extends Activity implements OnClickListener,
 		 */
 		while (state == State.RUNNING) {
 			try {
-				Connection conn;
-				Statement st;
-				ResultSet rs;
-
-				conn = DriverManager.getConnection(url, pgUser, pgPassword);
-
-				st = conn.createStatement();
-				rs = st
-						.executeQuery("SELECT NOW(), numbackends FROM pg_stat_database WHERE datname = '"
-								+ pgDatabase + "'");
-				if (rs.next()) {
-					headerString = pgDatabase + " " + rs.getString(1);
-					backendString = "Database Connections: " + rs.getString(2);
-				}
-				rs.close();
-				st.close();
-				conn.close();
+				getDatabaseStats();
 
 				handler.sendEmptyMessage(0);
 				// FIXME: Make the refresh rate a configuration parameter.
@@ -145,7 +224,26 @@ public class PGStatDatabase extends Activity implements OnClickListener,
 		@Override
 		public void handleMessage(Message msg) {
 			headerTextView.setText(headerString);
-			connectionsTextView.setText(backendString);
+			numbackendsTextView.setText("Database Connections: "
+					+ Long.toString(numbackends));
+			commitsTextView.setText("Commits: "
+					+ Long.toString(commits - commitsOld));
+			rollbacksTextView.setText("Rollbacks: "
+					+ Long.toString(rollbacks - rollbacksOld));
+			readTextView.setText("Blocks Read: "
+					+ Long.toString(read - readOld) + " (" + readPretty + ")");
+			hitTextView.setText("Blocks Hit: " + Long.toString(hit - hitOld)
+					+ " (" + hitPretty + ")");
+			returnedTextView.setText("Rows Returned: "
+					+ Long.toString(returned - returnedOld));
+			fetchedTextView.setText("Row Fetched: "
+					+ Long.toString(fetched - fetchedOld));
+			insertedTextView.setText("Rows Inserted: "
+					+ Long.toString(inserted - insertedOld));
+			updatedTextView.setText("Rows Updated: "
+					+ Long.toString(updated - updatedOld));
+			deletedTextView.setText("Rows Deleted: "
+					+ Long.toString(deleted - deletedOld));
 		}
 	};
 }
